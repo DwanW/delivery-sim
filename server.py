c61273b99c7dbc8d13a77da42dcd49b6cafcdfeb
DwanW <kunvang@yahoo.com>
2020-07-19 18:43:41 -0600

    c

--- server.py
+++ server.py
@@ -31,10 +31,10 @@ category_dict = {name:id for id, name in category_data}
 @app.route('/<path:path>')
 def serve(path):
     if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
+    	return send_from_directory(app.static_folder, path)
     else:
 		return jsonify({'message':'sending files'})
-        # return send_from_directory(app.static_folder, 'index.html')
+# return send_from_directory(app.static_folder, 'index.html')
 
 def token_required(f):
 	@wraps(f)
